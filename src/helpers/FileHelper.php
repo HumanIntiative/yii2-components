fdd56df74ec977e6206c5c211fad819756bd8c45
Zein Miftah <zeinmiftah@gmail.com>
2018-05-14 11:43:43 +0700

    Small fix for FileHelper

--- src/helpers/FileHelper.php
+++ src/helpers/FileHelper.php
@@ -3,7 +3,6 @@
 namespace pkpudev\components\helpers;
 
 use yii\helpers\FileHelper as YiiFileHelper;
-use yii\web\UploadedFile;
 
 /**
  * Formatter for Number and Numeric
@@ -15,32 +14,32 @@ class FileHelper extends YiiFileHelper
 	 * @return string Readable format
 	 */
 	public static function formatReadable($number)
-  {
-    $sizeSuffixes = ['B', 'KB', 'MB', 'GB'];
-    if ($number == 0) return "0 {$sizeSuffixes[0]}";
+	{
+		$sizeSuffixes = ['B', 'KB', 'MB', 'GB'];
+		if ($number == 0) return "0 {$sizeSuffixes[0]}";
 
-    $place = (int)floor(log($number, 1024));
-    $num   = round($number / pow(1024, $place), 2);
-    return "{$num} $sizeSuffixes[$place]";
-  }
+		$place = (int)floor(log($number, 1024));
+		$num   = round($number / pow(1024, $place), 2);
+		return "{$num} $sizeSuffixes[$place]";
+	}
 
-  /**
-   * @param string $ext Extension to favicon
-   * @return string favicon class
-   */
-  public static function getFaIcon($ext)
-  {
-    $ext = strtolower($ext);
+	/**
+	 * @param string $ext Extension to favicon
+	 * @return string favicon class
+	 */
+	public static function getFaIcon($ext)
+	{
+		$ext = strtolower($ext);
 
-    if (in_array($ext, ['csv', 'xls', 'xlsx'])) 				return 'fa fa-file-excel-o';
-    if (in_array($ext, ['doc', 'docx']))                return 'fa fa-file-word-o';
-    if (in_array($ext, ['gif', 'jpeg', 'jpg', 'png']))  return 'fa fa-file-image-o';
-    if (in_array($ext, ['pdf']))                        return 'fa fa-file-pdf-o';
-    if (in_array($ext, ['ppt','pptx']))                 return 'fa fa-file-powerpoint-o';
-    if (in_array($ext, ['rtf','txt']))                  return 'fa fa-file-text-o';
-    if (in_array($ext, ['zip','rar']))                  return 'fa fa-file-archive-o';
-    if (in_array($ext, ['folde']))                      return 'fa fa-folder-o';
+		if (in_array($ext, ['csv', 'xls', 'xlsx']))         return 'fa fa-file-excel-o';
+		if (in_array($ext, ['doc', 'docx']))                return 'fa fa-file-word-o';
+		if (in_array($ext, ['gif', 'jpeg', 'jpg', 'png']))  return 'fa fa-file-image-o';
+		if (in_array($ext, ['pdf']))                        return 'fa fa-file-pdf-o';
+		if (in_array($ext, ['ppt','pptx']))                 return 'fa fa-file-powerpoint-o';
+		if (in_array($ext, ['rtf','txt']))                  return 'fa fa-file-text-o';
+		if (in_array($ext, ['zip','rar']))                  return 'fa fa-file-archive-o';
+		if (in_array($ext, ['folde']))                      return 'fa fa-folder-o';
 
-    return 'fa fa-file-o';
-  }
+		return 'fa fa-file-o';
+	}
 }
