b9899b00f7effad3b1792f7049f9334f731f0b9c
Zein Miftah <zeinmiftah@gmail.com>
2019-02-04 16:55:47 +0700

    New bugfix EOF

--- src/web/DataTableJsExpression.php
+++ src/web/DataTableJsExpression.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace pkpudev\components\web;
 
@@ -8,67 +8,67 @@ use yii\web\View;
 
 class DataTableJsExpression extends BaseObject
 {
-    /**
-     * @var yii\web\View $view
-     */
-    public $view;
-    /**
-     * @var string $divSelector
-     */
-    public $divSelector;
-    /**
-     * @var string $addSelector
-     */
-    public $addSelector;
-    /**
-     * @var string $delSelector
-     */
-    public $delSelector;
-    /**
-     * @var integer $delIndex
-     */
-    public $delIndex;
-    /**
-     * @var yii\web\JsExpression $createdCallback
-     */
-    public $createdCallback;
-    /**
-     * @var yii\web\JsExpression $addedCallback
-     */
-    public $addedCallback;
-    /**
-     * @var yii\web\JsExpression $deletedCallback
-     */
-    public $deletedCallback;
+	/**
+	 * @var yii\web\View $view
+	 */
+	public $view;
+	/**
+	 * @var string $divSelector
+	 */
+	public $divSelector;
+	/**
+	 * @var string $addSelector
+	 */
+	public $addSelector;
+	/**
+	 * @var string $delSelector
+	 */
+	public $delSelector;
+	/**
+	 * @var integer $delIndex
+	 */
+	public $delIndex;
+	/**
+	 * @var yii\web\JsExpression $createdCallback
+	 */
+	public $createdCallback;
+	/**
+	 * @var yii\web\JsExpression $addedCallback
+	 */
+	public $addedCallback;
+	/**
+	 * @var yii\web\JsExpression $deletedCallback
+	 */
+	public $deletedCallback;
+	
+	public function init()
+	{
+		DataTableAsset::register($this->view);
+		
+		$irand = rand(0, 100);
+		$jsScript = <<<JS
+			// Var definitions
+			var divSelector = '{$this->divSelector}';
+			var addSelector = '{$this->addSelector}';
+			var delSelector = '{$this->delSelector}';
+			var delIndex    = {$this->delIndex} || 0;
 
-    public function init()
-    {
-        DataTableAsset::register($this->view);
-
-        $irand = rand(0, 100);
-        $jsScript = <<<JS
-            // Var definitions
-            var divSelector = '{$this->divSelector}';
-            var addSelector = '{$this->addSelector}';
-            var delSelector = '{$this->delSelector}';
-            var delIndex    = {$this->delIndex} || 0;
-
-            // Fn definitions
-            var createdCallback = function(row, data, index) {
-                {$this->createdCallback}
-            };
-            var addedCallback   = function() {
-                {$this->addedCallback}
-            };
-            var deletedCallback = function() {
-                {$this->deletedCallback}
-            };
-        JS;
-        $jsScript .= file_get_contents(__DIR__.'/datatable.js');
-        $jsScript = str_replace('varTable', "varTable{$irand}", $jsScript);
-        $jsScript = str_replace('varRowTable', "varRowTable{$irand}", $jsScript);
-        $jsScript = str_replace('delIndex', "delIndex{$irand}", $jsScript);
-
-        $this->view->registerJs($jsScript, View::POS_READY, "tabular-input-js{$irand}");
-    }
+			// Fn definitions
+			var createdCallback = function(row, data, index) {
+				{$this->createdCallback}
+			};
+			var addedCallback   = function() {
+				{$this->addedCallback}
+			};
+			var deletedCallback = function() {
+				{$this->deletedCallback}
+			};
+		JS;
+		$jsScript .= file_get_contents(__DIR__.'/datatable.js');
+		$jsScript = str_replace('varTable', "varTable{$irand}", $jsScript);
+		$jsScript = str_replace('varRowTable', "varRowTable{$irand}", $jsScript);
+		$jsScript = str_replace('delIndex', "delIndex{$irand}", $jsScript);
+		
+		$this->view->registerJs($jsScript, View::POS_READY, "tabula-input-js{$irand}");
+	}
 }
